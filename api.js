9326396fc63b0451e09a0d8b4551ad977a9e4377
mazpawel94 <mazpawel94@gmail.com>
2019-03-25 20:26:05 +0100

    second

--- api.js
+++ api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongodb = require('mongodb');
 
 const router = express.Router();
-
+router.get('/', (req,res) => {
+    res.send('Hello Word');
+})
 async function loadDB(collectionName) {
     const client = await mongodb.MongoClient.connect('mongodb://heroku_blbg8v1d:mqt4qpfkpned9r9mnk189v8f84@ds063439.mlab.com:63439/heroku_blbg8v1d', {
         useNewUrlParser: true
