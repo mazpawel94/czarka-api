5cfb849c8007da9adcdffd872b41739baf5e50ee
mazpawel94 <mazpawel94@gmail.com>
2019-03-28 13:49:57 +0100

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-const api = require('./api');
-app.use('/', api);
+const api = require("./api");
+app.use("/", api);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
-})
+  console.log(`server started on port ${port}`);
+});
