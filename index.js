6ab3b7f3e96dbadd58088790a48ef286c9567efa
mazpawel94 <mazpawel94@gmail.com>
2020-03-28 19:19:07 +0100

    add first socket

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+io.on("connect", socket => {
+  socket.emit('message', 'Welcome!');
+  //send to other users
+  socket.broadcast.emit("message", "new connected");
+})
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -10,6 +21,6 @@ const api = require("./api");
 app.use("/", api);
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
